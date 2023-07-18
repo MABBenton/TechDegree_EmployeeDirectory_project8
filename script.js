fab9dc7b8e35bada4937afb0b22bc7869bd9f143
MABBenton <mabbenton7@gmail.com>
2023-07-18 10:34:17 -0600

    created displayModal function

--- script.js
+++ script.js
@@ -40,3 +40,26 @@ function displayEmployees(employeeData) {
 
   gridContainer.innerHTML = employeeHTML;
 }
+
+//display modal function
+function displayModal(index) {
+  let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
+
+  let date = new Date(dob.date);
+
+  let modalHTML = `
+    <img class="avatar" src="${picture.large}"/>
+    <div class="text-container">
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p class="address">${phone}</p>
+        <p class="address">${street} ${state} ${postcode}</p> 
+        <p class="address">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    </div>
+  `;
+
+  overlay.classList.remove('hidden');
+  modalContainer.innerHTML = modalHTML;
+}
